74de007c5c28b84ddaf2e33a6f479e8fe34192ca
skotm <kodaka.shun0423@gmail.com>
2026-09-19 17:37:28 +0900

    Remove magnitude legend block from Legend component
    Removed the magnitude legend block from the Legend component.

--- src/components/Legend.jsx
+++ src/components/Legend.jsx
@@ -1,5 +1,3 @@
-import { pixelSizeForMag } from '../lib/color.js';
-
 export default function Legend() {
   return (
     <div className="panel legend">
@@ -7,20 +5,6 @@ export default function Legend() {
         <h3>深さ</h3>
         <div className="depth-gradient" />
         <div className="depth-ticks"><span>0km</span><span>350km</span><span>700km+</span></div>
-      </div>
-      <div className="legend-block">
-        <h3>マグニチュード</h3>
-        <div className="mag-legend">
-          {[3, 5, 7].map((mag) => {
-            const px = Math.round(pixelSizeForMag(mag));
-            return (
-              <div className="mag-item" key={mag}>
-                <div className="mag-dot" style={{ width: px, height: px }} />
-                <span>M{mag}</span>
-              </div>
-            );
-          })}
-        </div>
       </div>
     </div>
   );
